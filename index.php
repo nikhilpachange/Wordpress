073c72390248602e58b806ba47c9e2777551e7f3
nikhilpachange <nikhilpachange@gmail.com>
2024-12-10 00:32:18 +0530

    frist commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * main template file.
+ *
+ *
+ * @package Aquila
+ */
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wordpress theme</title>
+</head>
+<body>
+    
+</body>
+</html>
